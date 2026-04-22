525b5ca950b0bbafc1e897025a47096cbdb0efa1
Simon Worthington <simon@register-dynamics.co.uk>
2026-04-22 14:05:37 +1000

    Add search results from a prepared test database
    This commit uses a local SQLite3 database to run realistic searches as
    part of the prototype.
    
    The instructions for preparing the database are not yet included in
    this repository, and neither is the data. You will probably get an
    error if you try and run the prototype without an appropriate DB.
    
    For now we can pull the product name and manufacturer name for each
    search. We also include a routine to make up realistic and stable
    numbers for the associated Trusts and evidence. We use a stable PRNG
    so that numbers are consistent between searches for the same term and
    when we do the product page we can reflect the same numbers.

--- app/views/current/routes.js
+++ app/views/current/routes.js
@@ -1,3 +1,8 @@
+const allTrusts = require('./trusts')
+
+const Database = require('better-sqlite3')
+const db = new Database('test_db_pre_beta.db', { readonly: true })
+
 const router = require('express').Router()
 
 // Set version for all templates in this folder
@@ -6,6 +11,91 @@ router.use((req, res, next) => {
   next()
 })
 
-// Add any version-specific routes here
+
+// A moderate quality PNRG: https://gist.github.com/blixt/f17b47c62508be59987b?permalink_comment_id=2682175#gistcomment-2682175
+const mb32=a=>(t)=>(a=a+1831565813|0,t=Math.imul(a^a>>>15,1|a),t=t+Math.imul(t^t>>>7,61|t)^t,(t^t>>>14)>>>0)/2**32;
+const clamp_percent=(r,min,max)=>min+Math.round(max * r)
+
+function randomEvidence(model_id) {
+  const rand = mb32(model_id)
+
+  const numTrusts = clamp_percent(rand(), 0, 24)
+  const procured = new Set()
+  while (procured.size < numTrusts) {
+    procured.add(allTrusts[clamp_percent(rand(), 0, allTrusts.length-1)])
+  }
+
+  const numExcluded = clamp_percent(rand(), 0, Math.min(procured.size, 4))
+  const excluded = new Set()
+  while (excluded.size < numExcluded) {
+    let elem = Array.from(procured.keys())[clamp_percent(rand(), 0, procured.size-1)]
+    procured.delete(elem)
+    excluded.add(elem)
+  }
+
+  const numUnderReview = clamp_percent(rand(), 0, Math.min(procured.size, 6))
+  const underReview = new Set()
+  while (underReview.size < numUnderReview) {
+    let elem = Array.from(procured.keys())[clamp_percent(rand(), 0, procured.size-1)]
+    procured.delete(elem)
+    underReview.add(elem)
+  }
+
+  const documentTypes = ["Product trials", "Business cases", "Case studies"]
+  const numDocuments = clamp_percent(rand(), 0, Math.min(numTrusts * 3, 9))
+  const documents = new Array()
+  while (documents.length < numDocuments) {
+    documents.push(documentTypes[(clamp_percent(rand(), 0, documentTypes.length - 1))])
+  }
+
+  return {
+    trusts: new Set(procured).union(underReview).union(excluded),
+    procured: procured,
+    underReview: underReview,
+    excluded: excluded,
+    documents: documents,
+  }
+}
+
+const pageSize = 25
+const countQuery = db.prepare(`select COUNT(*) AS count FROM search WHERE search MATCH ?`)
+const searchQuery = db.prepare(`select MAKE_ID, MODEL_ID, DEVICE_ID, MAKE, MODEL, MANUFACTURER, CATEGORY, COUNTRY from search where search match @term limit @limit offset @offset`)
+router.get(/search-/, (req, res, next) => {
+  const term = req.query.q?.toString()
+  const page = parseInt(req.query.page || "1") - 1
+  const queryParams = {term: term, limit: pageSize, offset: pageSize * page}
+
+  const count = countQuery.raw(true).get(queryParams.term)
+  const results = searchQuery.all(queryParams)
+  console.log(`Found ${count} results (retrieved ${results.length}) for term ${term}`)
+
+  res.locals.searchTerm = term
+
+  res.locals.searchOffset = count > 0 ? (page * pageSize) + 1 : 0
+  res.locals.searchPage = page + 1
+  res.locals.searchMaxPages = Math.trunc(count / pageSize) + Math.min(count % pageSize, 1)
+  res.locals.searchResultsCount = count
+  res.locals.searchResults = results.map(function (result) {
+    const random = randomEvidence(result.MODEL_ID)
+
+    return {
+      make: result.MAKE,
+      make_id: result.MAKE_ID,
+      model: result.MODEL,
+      model_id: result.MODEL_ID,
+      device_id: result.DEVICE_ID,
+      manufacturer: result.MANUFACTURER,
+      category: result.CATEGORY,
+      country: result.COUNTRY,
+      trusts: random.trusts.size,
+      documents: random.documents.length,
+      document_types: Array.from(new Set(random.documents)).toSorted(),
+      procured: random.procured.size,
+      under_review: random.underReview.size,
+      excluded: random.excluded.size
+    }
+  })
+  next()
+})
 
 module.exports = router
