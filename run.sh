4a98bc3823eff1a2607b8b380d69bdbc1733edd0
Alaric Snell-Pym <alaric@register-dynamics.co.uk>
2026-07-07 15:40:05 +0100

    Adds scripts to build, and run, the site through a docker container; and replaces the generic Prototype Kit README with one that says what you need to do.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+./build.sh
+
+EXTRA_ARGS=""
+PORT=3000
+
+if [ $1 == "-p" ]
+then
+    PORT="$2"
+    EXTRA_ARGS="-p 127.0.0.1:$PORT:3000"
+    shift
+    shift
+fi
+
+echo "RUNNING THE SITE ON http://localhost:$PORT/"
+echo "Press Ctrl+C to stop it"
+
+docker run -it --rm --name dhsc-compass -v "$PWD":/compass $EXTRA_ARGS dhsc-compass
