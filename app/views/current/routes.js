5dacf58763b4e28878c64724e337875809f35f90
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-05 13:58:36 +0100

    Fix issues with search query - Missing comma - Search terms need to be treated as strings because of search being a virtual table

--- app/views/current/routes.js
+++ app/views/current/routes.js
@@ -109,7 +109,7 @@ router.get(/search-/, (req, res, next) => {
   next()
 })
 
-const individualQuery = db.prepare("select MAKE, MODEL, MODEL_ID, MANUFACTURER, GMDN_NAME TYPE, COUNTRY from search where MAKE_ID = ? and MODEL_ID = ? and DEVICE_ID = ?")
+const individualQuery = db.prepare("select MAKE, MODEL, MODEL_ID, MANUFACTURER, GMDN_NAME, TYPE, COUNTRY from search where MAKE_ID = ? and MODEL_ID = ? and DEVICE_ID = ?")
 
 const documentsQuery = db.prepare("SELECT organisation_name, type_of_doc_desc, rating, procured, scale, ward_department, assessment_date, expiry_date, org_category_desc, org_type_desc, url_directory FROM make_documents WHERE make_id = ?")
 
@@ -117,7 +117,7 @@ router.get(/product-page/, (req, res, next) => {
 
   console.log("GET product-page for make_id", req.query.make)
 
-  const result = individualQuery.get(parseInt(req.query.make), parseInt(req.query.model), parseInt(req.query.device))
+  const result = individualQuery.get(req.query.make, req.query.model, req.query.device)
   const random = randomEvidence(result.MODEL_ID)
 
   const documents = documentsQuery.all(parseInt(req.query.make))
