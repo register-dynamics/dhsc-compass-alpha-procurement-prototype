c19ac1da274191d75bbae5cc9b5cd85aa22d0ecb
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-13 15:54:16 +0100

    Add initital test framework

--- src/beta-app/eslint.config.mjs
+++ src/beta-app/eslint.config.mjs
@@ -3,6 +3,7 @@
 import js from "@eslint/js";
 import perfectionist from "eslint-plugin-perfectionist";
 import tseslint from "typescript-eslint";
+import vitestPlugin from "@vitest/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -21,4 +22,5 @@ export default defineConfig([
     },
   },
   perfectionist.configs["recommended-natural"],
+  vitestPlugin.configs.recommended,
 ]);
