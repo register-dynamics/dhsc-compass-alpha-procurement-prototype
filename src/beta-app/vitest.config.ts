16258f6b7e5cf86279993681c53d862aa8bc74c6
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-18 15:14:29 +0100

    Exclude client-side assets from server-side tests

--- src/beta-app/vitest.config.ts
+++ src/beta-app/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ["index.ts", "**/*.config.*", "**/*.test.*"],
+      exclude: ["index.ts", "**/*.config.*", "**/*.test.*", "assets/**", "public/**"],
       provider: "v8",
       reporter: ["text", "lcov", "cobertura", "json", "json-summary"],
     },
