aa81d3b246f05e169b12a179ddd81ce5a7e03b76
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-18 11:51:08 +0100

    Add frontend with css via sass

--- src/beta-app/app.ts
+++ src/beta-app/app.ts
@@ -1,13 +1,35 @@
 import express from "express";
+import nunjucks from "nunjucks";
 
 import config from "./config.js";
 
 const app = express();
 
+// Set up Nunjucks templating engine with NHS Design System
+nunjucks.configure(
+  [
+    "node_modules/nhsuk-frontend/dist/nhsuk/components",
+    "node_modules/nhsuk-frontend/dist/nhsuk/macros",
+    "node_modules/nhsuk-frontend/dist/nhsuk",
+    "node_modules/nhsuk-frontend/dist",
+    "views"
+  ],
+  {
+    autoescape: true,
+    express: app,
+  },
+);
+
+app.set("view engine", "html");
+
 app.use(express.json());
 
+// Serve static files from the public directory§
+const publicDir = new URL("./public", import.meta.url).pathname;
+app.use(express.static(publicDir));
+
 app.get("/", (req, res) => {
-  res.send(`Hello, world! Welcome to ${config.app.name}.`);
+  res.render("index.html", { appName: config.app.name });
 });
 
 export default app;
