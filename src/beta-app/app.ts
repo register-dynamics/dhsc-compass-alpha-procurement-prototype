672da068a9a6b82244031c725cf3fb186263d2ba
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-18 14:57:16 +0100

    Update comments

--- src/beta-app/app.ts
+++ src/beta-app/app.ts
@@ -26,10 +26,11 @@ app.set("view engine", "html");
 
 app.use(express.json());
 
-// Serve static files from the public directory§
+// Serve static files from the public directory
 const publicDir = new URL("./public", import.meta.url).pathname;
 app.use(express.static(publicDir));
 
+// Hook-in to node_modules to serve the NHS Design System assets at /assets
 app.use('/assets', express.static("node_modules/nhsuk-frontend/dist/nhsuk/assets"));
 
 app.get("/", (req, res) => {
