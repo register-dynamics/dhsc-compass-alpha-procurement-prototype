1ebccdbc616c7099a97223aec9265c80967f5e05
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-19 13:47:38 +0100

    Add static search page

--- src/beta-app/app.ts
+++ src/beta-app/app.ts
@@ -37,7 +37,12 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.render("index.html", { appName: config.app.name });
+  res.render("index.html");
 });
+
+app.get("/search", (req, res) => {
+  res.render("search.html");
+});
+
 
 export default app;
