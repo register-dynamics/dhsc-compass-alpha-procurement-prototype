9e50ddab2b88d149d435f6b1ac93bbadf55f92e5
Jack Gilmore <gilmorejackc@gmail.com>
2026-08-21 16:38:14 +0100

    Fix tests

--- src/beta-app/controllers/search.controller.test.ts
+++ src/beta-app/controllers/search.controller.test.ts
@@ -1,25 +1,48 @@
 import request from "supertest";
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 
 import app from "../app.js";
 
+// TODO: Store the test user config and prep in a more appropriate place that can be shared across tests
+const testUser = {
+  password: "northsouth",
+  username: "test@example.com",
+};
+
+const loginAndGetCookie = async () => {
+  const response = await request(app)
+    .post("/sign-in")
+    .send(testUser)
+    .set("Content-Type", "application/x-www-form-urlencoded");
+
+  const cookies = response.headers["set-cookie"];
+  return cookies;
+};
+
 describe("Beta app server (integration)", () => {
+
+  let cookies: string;
+
+  beforeEach(async () => {
+    cookies = await loginAndGetCookie();
+  });
+
   it("GET /search should return 200 and search page", async () => {
-    const response = await request(app).get("/search");
+    const response = await request(app).get("/search").set("Cookie", cookies);
 
     expect(response.status).toBe(200);
     expect(response.text).toContain("Search");
   });
 
   it("GET /search-results should return 200 and search results page", async () => {
-    const response = await request(app).get("/search-results?q=test");
+    const response = await request(app).get("/search-results?q=test").set("Cookie", cookies);
 
     expect(response.status).toBe(200);
     expect(response.text).toContain("Search results");
   });
 
   it("GET /search-results with empty search should return 200 and search results page", async () => {
-    const response = await request(app).get("/search-results");
+    const response = await request(app).get("/search-results").set("Cookie", cookies);
 
     expect(response.status).toBe(200);
     expect(response.text).toContain("Search results");
@@ -28,7 +51,7 @@ describe("Beta app server (integration)", () => {
   it("GET /search-results with categories should return 200 and search results page", async () => {
     const response = await request(app).get(
       "/search-results?q=test&[category]=category1",
-    );
+    ).set("Cookie", cookies);
 
     expect(response.status).toBe(200);
     expect(response.text).toContain("Search results");
