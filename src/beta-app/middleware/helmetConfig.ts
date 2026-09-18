72d0c13dfcd8c4ec946110c039c5b0f1b90b9b9c
Jack Gilmore <gilmorejackc@gmail.com>
2026-09-18 13:24:16 +0100

    Add security headers with helmet.js package

---
+++ src/beta-app/middleware/helmetConfig.ts
@@ -0,0 +1,22 @@
+import type { HelmetOptions } from "helmet";
+
+export const helmetConfig: HelmetOptions = {
+  // Content Security Policy (CSP): Restrict the sources from which various types of content can be loaded to enhance security
+  contentSecurityPolicy: {
+    directives: {
+      connectSrc: ["'self'"],
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", "data:", "https://assets.nhs.uk"],
+      imgSrc: ["'self'", "data:"],
+      objectSrc: ["'none'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+    },
+  },
+  // Referrer Policy: Control the amount of referrer information sent with requests to enhance privacy and security
+  referrerPolicy: {
+    policy: "strict-origin-when-cross-origin",
+  },
+  // X-Frame-Options: Prevent the site from being framed to protect against clickjacking attacks
+  xFrameOptions: { action: "deny" },
+};
